e25e1b523905d660cfd5298182f0917ca9f2b160
Blake <blakemealey@gmail.com>
2017-09-06 19:24:18 -0600

    Updated resume

--- routes/index.js
+++ routes/index.js
@@ -5,45 +5,41 @@ var converter = require('number-to-words');
 var express = require('express');
 var router = express.Router();
 
-function readFile(fileName, callback) {
-	var data = fs.readFileSync(fileName, "utf8");
+var dataDirectory = "views/data/";
+function readFile(fileName) {
+	var data = fs.readFileSync(dataDirectory + fileName, "utf8");
 	return JSON.parse(JSON.minify(data)); // minify so comments work in data files
 }
-
-var dataDirectory = "views/data/";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	// Read in initial data from files
 	var data = {
 		title: "Blake Mealey Resume",
-		links: readFile(dataDirectory + "links.json"),
-		programming: readFile(dataDirectory + "programming.json"),
-		software: readFile(dataDirectory + "software.json"),
-		competitions: readFile(dataDirectory + "competitions.json"),
-		experience: readFile(dataDirectory + "experience.json"),
-		education: readFile(dataDirectory + "education.json"),
-		activities: readFile(dataDirectory + "activities.json")
+		links: readFile("links.json"),
+		programming: readFile("programming.json"),
+		software: readFile("software.json"),
+		competitions: readFile("competitions.json"),
+		experience: readFile("experience.json"),
+		education: readFile("education.json"),
+		activities: readFile("activities.json")
 	};
 
 	// Determine bar length and number of years experience for programming languages
-	var currentYear = new Date().getFullYear();
-	var longestYearsPassed = 0;
-	var language;
+	var mostYears = 0, language;
 	for(var i = data.programming.length - 1; i >= 0; i--) {
 		language = data.programming[i];
-		language.yearsPassed = currentYear - language.firstYear;
-		longestYearsPassed = Math.max(longestYearsPassed, language.yearsPassed);
+		mostYears = Math.max(mostYears, language.years);
 	}
 
 	for(i = data.programming.length - 1; i >= 0; i--) {
 		language = data.programming[i];
-		language.barValue = 100*(language.yearsPassed/longestYearsPassed) + "%";
-		var wordNumber = converter.toWords(language.yearsPassed);
+		language.barValue = 100*(language.years/mostYears) + "%";
+		var wordNumber = converter.toWords(language.years);
 
 		language.description = (language.yearsOverride ? language.yearsOverride : wordNumber.charAt(0).toUpperCase() +
-			wordNumber.slice(1).toLowerCase() + " year" + (language.yearsPassed == 1 ? "" : "s") + " experience") +
-			(language.description ? language.description : "");
+			wordNumber.slice(1).toLowerCase() + " year" + (language.years == 1 ? "" : "s")) + " experience" +
+			(language.description ? ", " + language.description : "");
 	}
 
 	// Render the page
--- views/markdown/skills-achievements.md
+++ views/markdown/skills-achievements.md
@@ -1,5 +1,8 @@
 ### Competitions
-- Second place of the University of Calgary teams for the Alberta Collegiate Programming Contest (ACPC) 2016, seventh place overall, on a team of three
-- Third place in the 2016 hackathon at University of Calgary, on a team of four
-- Winner of Division II for the Calgary Collegiate Programming Contest (CCPC) 2015, on a team of three
-- Participated in many other competitions including USACO, Pason Coding Contests, ACPC 2015, and CCPC 2016 among others
+- Third place of Division I for the Calgary Collegiate Programming Contest (CCPC) 2017, on a team of three
+- Third place in the 2016 hackathon at UofC, on a team of four
+- Winner of Division II for the CCPC 2015, on a team of three
+
+### Academic Achievements
+- On the UofC Faculty of Science Dean's List [2014-2015](https://www.ucalgary.ca/science/excellence_awards/deans_list_2014_2015), [2015-2016](https://www.ucalgary.ca/science/excellence_awards/deans_list_2015_2016), and [2016-2017](https://www.ucalgary.ca/science/excellence_awards/deans_list_2016_2017)
+- Received a University of Calgary Undergraduate Merit Award, 2016
