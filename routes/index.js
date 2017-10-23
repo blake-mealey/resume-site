fd2b185bb5c74f053e09e2e1fddc612237742b1d
Blake Mealey <blakemealey@gmail.com>
2017-10-23 21:08:37 +0000

    Updated to use resume-data repo

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
 var fs = require('fs');
 var jsonminify = require('jsonminify');
 var converter = require('number-to-words');
+var request = require('request');
+var path = require('path');
+var converter = require('number-to-words');
 
 var express = require('express');
 var router = express.Router();
 
 var dataDirectory = "views/data/";
-function readFile(fileName) {
-	var data = fs.readFileSync(dataDirectory + fileName, "utf8");
+var secretsDirectory = "secrets/";
+function readFile(fileName, dir) {
+	dir = dir || dataDirectory;
+	var data = fs.readFileSync(path.join(dir, fileName), "utf8");
 	return JSON.parse(JSON.minify(data)); // minify so comments work in data files
 }
 
@@ -17,17 +22,27 @@ router.get('/', function(req, res, next) {
 	var data = {
 		title: "Blake Mealey Resume",
 		links: readFile("links.json"),
-		programming: readFile("programming.json"),
-		software: readFile("software.json"),
-		competitions: readFile("competitions.json"),
 		experience: readFile("experience.json"),
 		education: readFile("education.json"),
+		projects: readFile("projects.json"),
+		programming: readFile("programming.json"),
+		software: readFile("software.json"),
+		miscellaneous: readFile("miscellaneous.json"),
+		achievements: readFile("achievements.json"),
 		activities: readFile("activities.json")
 	};
 
+	for (var i = 0; i < data.achievements.competitions.length; i++) {
+		var ordinal = converter.toOrdinal(data.achievements.competitions[i].place);
+		data.achievements.competitions[i].place = {
+			num: ordinal.match(/[0-9]+/),
+			postfix: ordinal.match(/[a-z]+/)
+		}
+	}
+
 	// Determine bar length and number of years experience for programming languages
 	var mostYears = 0, language;
-	for(var i = data.programming.length - 1; i >= 0; i--) {
+	for(i = data.programming.length - 1; i >= 0; i--) {
 		language = data.programming[i];
 		mostYears = Math.max(mostYears, language.years);
 	}
@@ -46,4 +61,47 @@ router.get('/', function(req, res, next) {
 	res.render('index', data);
 });
 
+var githubUser = readFile("githubUser.json", secretsDirectory);
+var repoHost = 'https://raw.githubusercontent.com/';
+var repoPath = githubUser.user + '/resume-data/master/';
+var authHeaders = {
+	Authorization: "Basic " + new Buffer(githubUser.user + ":" + githubUser.pass).toString("base64")
+}
+
+/* Update page data */
+router.post('/update', function(req, res, next) {
+	console.log("Updating data files.");
+	res.send();
+
+	setTimeout(doUpdateData, 2000);
+});
+
+function doUpdateData() {
+	request.get({
+		url: repoHost + path.join(repoPath, "header.json"),
+		json: true,
+		headers: authHeaders
+	}, function(err, res, body) {
+		if (err) return console.log("Error downloading header file: " + err);
+		console.log(body.files);
+		function getFile(fileName) {
+			request
+				.get({
+					url: repoHost + path.join(repoPath, fileName),
+					headers: authHeaders
+				}).on('error', function(err) {
+					console.log("Error downloading file '" + fileName + "': " + err);
+				})
+				.pipe(fs.createWriteStream(path.join(dataDirectory, fileName)))
+				.on('finish', function() {
+					console.log("Downloaded file '" + fileName + "'");
+				});
+		}
+		for (var i = 0; i < body.files.length; i++) {
+			getFile(body.files[i]);
+		}
+	});
+}
+
+doUpdateData();
 module.exports = router;
